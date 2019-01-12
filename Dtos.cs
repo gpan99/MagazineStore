925ff3c40a1f220a09795e972e708dac757aecaf
George Pan <gpan99@yahoo.com>
2019-01-12 12:09:43 -0500

    manual merge

--- Dtos.cs
+++ Dtos.cs
@@ -29,4 +29,37 @@ namespace WebAPIClient
         public bool success;
         public string token;
     }
+    public class ResponseData
+    {
+        public string totalTime { get; set; }
+        public bool answerCorrect { get; set; }
+        public List<string> shouldBe { get; set; }
+    }
+
+    public class Response
+    {
+        public ResponseData data { get; set; }
+        public bool success { get; set; }
+        public string token { get; set; }
+        public string message { get; set; }
+    }
+    public class Datum
+    {
+        public string id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public List<int> magazineIds { get; set; }
+    }
+
+    public class Subscriber
+    {
+        public List<Datum> data { get; set; }
+        public bool success { get; set; }
+        public string token { get; set; }
+        public string message { get; set; }
+    }
+    public class Answer
+    {
+        public List<string> subscribers { get; set; }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,47 +1,87 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WebAPIClient
 {
     class Program
     {
-        private static readonly HttpClient client = new HttpClient();
+        private static HttpClient client = new HttpClient();
+        private static string apiUrl = "http://magazinestore.azurewebsites.net/api/";
 
         static void Main(string[] args)
         {
             client.DefaultRequestHeaders.Add("Accept", "application/json");
-            string apiUrl = "http://magazinestore.azurewebsites.net/api/";
+
             var token = GetToken(apiUrl + "token").Result.token;
-            var categories = GetCategories(apiUrl + "categories/"+token).Result.data;
-            var magazines = GetMagazines(apiUrl + "categories/" + token + "magazines/" + categories[0]).Result;
-        }
-        private static async Task<Magazine> GetMagazines(string apiUrl)
-        {
-            var serializer = new DataContractJsonSerializer(typeof(Magazine));
+            var subscribers = GetSubscribers(apiUrl + "subscribers/" + token).data;
 
+            // Get subscriber Ids
+
+            List<string> subscriberIds = new List<string>();
+            foreach (var s in subscribers)
+            { subscriberIds.Add(s.id); }
+
+            // create answer model
+
+            var answer = new Answer { subscribers = subscriberIds };
+            string json = JsonConvert.SerializeObject(answer);
+
+            // Post answer, it returns answerCorrect = false with should be list
+
+            var resp = PostAnswer(token, json);
+
+            // repost with should be list
+            answer = new Answer { subscribers = resp.data.shouldBe };
+            json = JsonConvert.SerializeObject(answer);
+
+            // will return answerCorrect = true with should be list = null
+            resp = PostAnswer(token, json);
+
+            Console.ReadLine();
+        }
+        public static Response PostAnswer(string token, string json)
+        {
+            string URL = apiUrl + "answer/" + token;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(URL);
+            httpWebRequest.ContentType = "application/json";
+
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                Console.WriteLine("json: " + json);
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                Console.WriteLine("result: " + result.ToString());
+                return JsonConvert.DeserializeObject<Response>(result.ToString());
+            }
+        }
+        private static Subscriber GetSubscribers(string apiUrl)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(Subscriber));
             var streamTask = client.GetStreamAsync(apiUrl).Result;
-            var Magazines = serializer.ReadObject(streamTask) as Magazine;
-            return Magazines;
+            return serializer.ReadObject(streamTask) as Subscriber;
         }
 
-        private static async Task<Category> GetCategories(string apiUrl)
-        {
-            var serializer = new DataContractJsonSerializer(typeof(Category));
-          
-            var streamTask = client.GetStreamAsync(apiUrl).Result;
-            var categories = serializer.ReadObject(streamTask) as Category;
-            return categories;
-        }
         private static async Task<Token> GetToken(string apiUrl)
         {
-            var serializer = new DataContractJsonSerializer(typeof(Token));         
+            var serializer = new DataContractJsonSerializer(typeof(Token));
             var streamTask = client.GetStreamAsync(apiUrl).Result;
-            var token = serializer.ReadObject(streamTask) as Token;
-            return token;
+            return serializer.ReadObject(streamTask) as Token;
         }
     }
 }
